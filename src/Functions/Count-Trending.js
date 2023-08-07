2128190cc68cdf595f55a18e2067ceb7330c485e
AryanAg08 <aryangoyal90@outlook.com>
2023-08-07 22:31:18 +0530

    view count updated function

---
+++ src/Functions/Count-Trending.js
@@ -0,0 +1,26 @@
+const schedule = require("node-schedule");
+const B1 = require("../Models/3.BlogPosts");
+
+
+async function MakeViewsCorrect () {
+
+var j = schedule.scheduleJob("0 0 0 * * *", async function () {
+
+    const B2  = await B1.find();
+
+    for (qq of B2) {
+       const v  = qq.views;
+       const idd = qq._id;
+
+          const NvE = math.round(v / 2);
+
+          const B3 = await B1.findByIdAndUpdate({
+            _id: idd,
+          },{
+            views: NvE,
+          },{
+            upsert: true,
+          });
+       }
+})
+}
